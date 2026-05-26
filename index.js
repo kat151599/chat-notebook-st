1e46f92f7fb39b947f9968677c0243d3a8524477
kat151599 <126671388+kat151599@users.noreply.github.com>
2026-05-26 16:26:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -25,3 +25,33 @@ function addNotebookButton() {
 
     $("body").append(btn);
 }
+function addNotebookModal() {
+    if ($("#chat_notebook_modal").length) return;
+
+    const modal = $(`
+        <div id="chat_notebook_modal"
+             style="
+                display:none;
+                position: fixed;
+                right: 20px;
+                bottom: 170px;
+                width: 300px;
+                height: 400px;
+                background: var(--SmartThemeBlurTintColor);
+                border: 1px solid var(--SmartThemeBorderColor);
+                border-radius: 12px;
+                z-index: 9999;
+                padding: 10px;
+             ">
+            <textarea id="chat_notebook_textarea"
+                style="width:100%; height:100%; resize:none;"></textarea>
+        </div>
+    `);
+
+    $("body").append(modal);
+}
+jQuery(async () => {
+    addNotebookButton();
+    addNotebookModal();
+    loadChatNote();
+});
